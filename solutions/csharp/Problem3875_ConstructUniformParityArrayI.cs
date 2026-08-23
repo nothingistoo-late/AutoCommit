769a625f4f9b8fe19831b674a3c62e02af9c2669
hctrung2k4 <hctrung2k4@gmail.com>
2026-08-23 12:26:48 +0700

    feat(csharp): solve LeetCode 3875 - 'Construct Uniform Parity Array I' (Easy)

--- solutions/csharp/Problem3875_ConstructUniformParityArrayI.cs
+++ solutions/csharp/Problem3875_ConstructUniformParityArrayI.cs
@@ -4,7 +4,7 @@ namespace AutoCommit.Solutions;
 /// LeetCode 3875: Construct Uniform Parity Array I
 /// Difficulty: Easy | Tags: Array, Math
 /// Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
-/// Solved on: 2026-08-22 12:32:43
+/// Solved on: 2026-08-23 12:26:48
 /// </summary>
 public class Problem3875_ConstructUniformParityArrayI
 {

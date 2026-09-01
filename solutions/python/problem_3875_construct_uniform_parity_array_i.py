7af30751363cef71376648c0c3f8d9d16f4dd5b4
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-01 16:07:39 +0700

    feat(python): solve LeetCode 3875 - 'Construct Uniform Parity Array I' (Easy)

--- solutions/python/problem_3875_construct_uniform_parity_array_i.py
+++ solutions/python/problem_3875_construct_uniform_parity_array_i.py
@@ -1,7 +1,7 @@
 # LeetCode 3875: Construct Uniform Parity Array I
 # Difficulty: Easy | Tags: Array, Math
 # Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
-# Solved on: 2026-08-31 15:08:12
+# Solved on: 2026-09-01 16:07:39
 
 class Solution:
     def solve(self, s: str, target: str) -> str:

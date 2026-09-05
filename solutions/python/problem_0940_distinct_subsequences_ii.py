a954bed9dfbfc0388367d6dcccc49d3b7644b983
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-05 16:55:32 +0700

    feat(python): solve LeetCode 940 - 'Distinct Subsequences II' (Hard)

--- solutions/python/problem_0940_distinct_subsequences_ii.py
+++ solutions/python/problem_0940_distinct_subsequences_ii.py
@@ -1,7 +1,7 @@
 # LeetCode 940: Distinct Subsequences II
 # Difficulty: Hard | Tags: String, Dynamic Programming
 # Link: https://leetcode.com/problems/distinct-subsequences-ii/
-# Solved on: 2026-09-04 15:47:14
+# Solved on: 2026-09-05 16:55:32
 
 class Solution:
     def solve(self, s: str, target: str) -> str:

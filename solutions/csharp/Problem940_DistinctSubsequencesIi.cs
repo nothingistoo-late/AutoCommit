414668b9e5bc400c6d058526adc2718088d7d655
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-07 08:52:08 +0700

    feat(csharp): solve LeetCode 940 - 'Distinct Subsequences II' (Hard)

--- solutions/csharp/Problem940_DistinctSubsequencesIi.cs
+++ solutions/csharp/Problem940_DistinctSubsequencesIi.cs
@@ -4,7 +4,7 @@ namespace AutoCommit.Solutions;
 /// LeetCode 940: Distinct Subsequences II
 /// Difficulty: Hard | Tags: String, Dynamic Programming
 /// Link: https://leetcode.com/problems/distinct-subsequences-ii/
-/// Solved on: 2026-09-07 08:43:10
+/// Solved on: 2026-09-07 08:52:08
 /// </summary>
 public class Problem940_DistinctSubsequencesIi
 {

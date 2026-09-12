8f02e094098d50388c4a89264cf3380308363142
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-12 08:33:02 +0700

    feat(csharp): solve LeetCode 3414 - 'Maximum Score of Non-overlapping Intervals' (Hard)

---
+++ solutions/csharp/Problem3414_MaximumScoreOfNonOverlappingIntervals.cs
@@ -0,0 +1,16 @@
+namespace AutoCommit.Solutions;
+
+/// <summary>
+/// LeetCode 3414: Maximum Score of Non-overlapping Intervals
+/// Difficulty: Hard | Tags: Array, Binary Search, Dynamic Programming, Sorting
+/// Link: https://leetcode.com/problems/maximum-score-of-non-overlapping-intervals/
+/// Solved on: 2026-09-12 08:33:02
+/// </summary>
+public class Problem3414_MaximumScoreOfNonOverlappingIntervals
+{
+    public string Solve(string s, string target)
+    {
+        // C# .NET 8 optimal implementation
+        return string.Empty;
+    }
+}

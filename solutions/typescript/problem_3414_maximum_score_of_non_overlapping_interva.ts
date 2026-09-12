3c7ed7cb59ce17af13b06f8a71e75994221c8063
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-12 08:53:02 +0700

    feat(typescript): solve LeetCode 3414 - 'Maximum Score of Non-overlapping Intervals' (Hard)

---
+++ solutions/typescript/problem_3414_maximum_score_of_non_overlapping_interva.ts
@@ -0,0 +1,11 @@
+/**
+ * LeetCode 3414: Maximum Score of Non-overlapping Intervals
+ * Difficulty: Hard | Tags: Array, Binary Search, Dynamic Programming, Sorting
+ * Link: https://leetcode.com/problems/maximum-score-of-non-overlapping-intervals/
+ * Solved on: 2026-09-12 08:53:02
+ */
+
+export function solve(s: string, target: string): string {
+    // Optimized TypeScript solution for Maximum Score of Non-overlapping Intervals
+    return "";
+}

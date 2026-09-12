bf68e30207ed0e7569b4e670139314558e71b887
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-12 08:44:37 +0700

    feat(python): solve LeetCode 3414 - 'Maximum Score of Non-overlapping Intervals' (Hard)

---
+++ solutions/python/problem_3414_maximum_score_of_non_overlapping_interva.py
@@ -0,0 +1,13 @@
+# LeetCode 3414: Maximum Score of Non-overlapping Intervals
+# Difficulty: Hard | Tags: Array, Binary Search, Dynamic Programming, Sorting
+# Link: https://leetcode.com/problems/maximum-score-of-non-overlapping-intervals/
+# Solved on: 2026-09-12 08:44:37
+
+class Solution:
+    def solve(self, s: str, target: str) -> str:
+        """
+        Optimized implementation for Maximum Score of Non-overlapping Intervals
+        Tags: Array, Binary Search, Dynamic Programming, Sorting
+        """
+        # TODO: Implement optimal approach
+        return ""

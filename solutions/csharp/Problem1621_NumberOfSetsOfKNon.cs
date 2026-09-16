06fc39bcfee8de6b6418e7f79c3d7f0fca66b82e
hctrung2k4 <hctrung2k4@gmail.com>
2026-09-16 08:58:39 +0700

    feat(csharp): solve LeetCode 1621 - 'Number of Sets of K Non-Overlapping Line Segments' (Medium)

---
+++ solutions/csharp/Problem1621_NumberOfSetsOfKNon.cs
@@ -0,0 +1,16 @@
+namespace AutoCommit.Solutions;
+
+/// <summary>
+/// LeetCode 1621: Number of Sets of K Non-Overlapping Line Segments
+/// Difficulty: Medium | Tags: Math, Dynamic Programming, Combinatorics, Prefix Sum
+/// Link: https://leetcode.com/problems/number-of-sets-of-k-non-overlapping-line-segments/
+/// Solved on: 2026-09-16 08:58:39
+/// </summary>
+public class Problem1621_NumberOfSetsOfKNon
+{
+    public string Solve(string s, string target)
+    {
+        // C# .NET 8 optimal implementation
+        return string.Empty;
+    }
+}
